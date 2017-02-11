6a44445c5e052024035463b07bd73f95aab534d6
waynedw <greyman@live.cn>
2017-02-11 14:29:09 -0500

    .

--- code/initial_v9_grid_feature_importance.py
+++ code/initial_v9_grid_feature_importance.py
@@ -8,7 +8,8 @@ import pandas as pd
 from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression, Ridge
 
-w1 = float(sys.argv[1])
+#w1 = float(sys.argv[1])
+w1 = 0.8
 w2 = 1 - w1
 
 
@@ -46,10 +47,15 @@ feature_group3 = ['fundamental_24_nan_','fundamental_25_nan_','fundamental_26_na
 
 feature_group4 = ['fundamental_40_nan_','fundamental_41_nan_','fundamental_42_nan_','fundamental_43_nan_','fundamental_44_nan_','fundamental_45_nan_','fundamental_46_nan_','fundamental_47_nan_','fundamental_48_nan_','fundamental_49_nan_','fundamental_50_nan_','fundamental_51_nan_','fundamental_52_nan_','fundamental_53_nan_','fundamental_54_nan_','fundamental_55_nan_','fundamental_56_nan_','fundamental_57_nan_']
 
-top_features = feature_group0 + feature_group1 + feature_group2
+
+feature_depth_4_0 = ['derived_0','derived_1','derived_2','derived_3','derived_4','fundamental_0','fundamental_1','fundamental_2','fundamental_3','fundamental_5','fundamental_6','fundamental_7','fundamental_8','fundamental_9','fundamental_10','fundamental_11','fundamental_12','fundamental_13','fundamental_14','fundamental_15','fundamental_16','fundamental_17','fundamental_18','fundamental_19','fundamental_20','fundamental_21','fundamental_22','fundamental_23','fundamental_24','fundamental_25','fundamental_26','fundamental_27','fundamental_28','fundamental_29','fundamental_30','fundamental_31','fundamental_32','fundamental_33','fundamental_34','fundamental_35','fundamental_36','fundamental_37','fundamental_38','fundamental_39','fundamental_40','fundamental_41','fundamental_42','fundamental_43','fundamental_44','fundamental_45','fundamental_46','fundamental_47','fundamental_48','fundamental_49','fundamental_50','fundamental_51','fundamental_52','fundamental_53','fundamental_54','fundamental_55','fundamental_56','fundamental_57','fundamental_58','fundamental_59','fundamental_60','fundamental_61','fundamental_62','fundamental_63','technical_0','technical_1','technical_2','technical_3','technical_5','technical_6','technical_7','technical_9','technical_10','technical_11','technical_12','technical_13','technical_14','technical_16','technical_17','technical_18','technical_19','technical_20','technical_21','technical_22','technical_24','technical_25','technical_27','technical_28','technical_29','technical_30','technical_31','technical_32','technical_33','technical_34','technical_35','technical_36','technical_37','technical_38','technical_39','technical_40','technical_41','technical_42','technical_43','technical_44']
+
+feature_depth_4_1 = ['derived_0_nan_','derived_1_nan_','derived_2_nan_','derived_3_nan_','derived_4_nan_','fundamental_0_nan_','fundamental_1_nan_','fundamental_2_nan_','fundamental_3_nan_','fundamental_5_nan_','fundamental_6_nan_','fundamental_7_nan_','fundamental_8_nan_','fundamental_9_nan_','fundamental_10_nan_','fundamental_11_nan_','fundamental_12_nan_','fundamental_13_nan_','fundamental_14_nan_','fundamental_15_nan_','fundamental_16_nan_','fundamental_17_nan_','fundamental_18_nan_','fundamental_19_nan_','fundamental_20_nan_','fundamental_21_nan_','fundamental_22_nan_','fundamental_23_nan_','fundamental_24_nan_','fundamental_25_nan_','fundamental_26_nan_','fundamental_27_nan_','fundamental_28_nan_','fundamental_29_nan_','fundamental_30_nan_','fundamental_31_nan_']
+
+top_features = feature_depth_4_0 + feature_depth_4_1
 
 
-rfr = ExtraTreesRegressor(n_estimators=100, max_depth=5, n_jobs=-1, random_state=17, verbose=0)
+rfr = ExtraTreesRegressor(n_estimators=100, max_depth=4, n_jobs=-1, random_state=17, verbose=0)
 model1 = rfr.fit(train[top_features], data.train['y']) # train model 1
 
 
@@ -58,7 +64,6 @@ std = np.std([tree.feature_importances_ for tree in rfr.estimators_],
              axis=0)
 indices = np.argsort(importances)[::-1]
 
-'''
 # Print the feature ranking
 print("Feature ranking:")
 
@@ -69,7 +74,6 @@ try:
         print("%d. feature %d   feature name %s, importance (%f)" % (f + 1, indices[f],  train.columns[f], importances[indices[f]]))
 except:
     pass
-'''
 
 # https://www.kaggle.com/bguberfain/two-sigma-financial-modeling/univariate-model-with-clip/run/482189
 # predict based on one-index factor model
@@ -112,3 +116,4 @@ while True:
         break
     if data.features.timestamp[0] % 100 == 0:
         print(reward)
+
